c8cbdbc778d4e478c5d55f0da3e3238044d0c95d
Walker Chan <wrchan@mit.edu>
2007-03-11 02:55:41 +0000

    Moved remotely

---
+++ boards2/logic/LogicCode/4Enet.h
@@ -0,0 +1,37 @@
+// === BemixNet ===
+
+#ifndef __BEMIXNET_H
+#define __BEMIXNET_H
+
+#define MAX_PACKET_SIZE 20
+
+#define ESCAPE_CODE '\\'
+#define HEADER_CODE 'H'
+#define END_CODE    'E'
+
+#define MY_ADDRESS  '2'
+
+//#define TX_ENABLE PORTAbits.RA0
+
+typedef struct {
+	unsigned char destination;
+	unsigned char address;							// source for rx, destination for tx
+	unsigned char port;								//'w' = wheels
+	unsigned char data[MAX_PACKET_SIZE];
+	unsigned length:7;
+	unsigned done:1;
+} PacketBuffer;
+
+enum ReceiveState {ESCAPE, HEADER, SOURCE, PORT, DATA, DATAESC, IGNORE};
+
+void handleRx(PacketBuffer * RxPacket);
+
+void handleTx();
+void transmit();
+
+void initRx(PacketBuffer * RxPacket);
+void initTx(PacketBuffer TxPacket);
+
+void delay();
+
+#endif
---
+++ boards2/logic/LogicCode/kicker.c
@@ -0,0 +1,96 @@
+// kicker.c
+
+#include "kicker.h"
+#include "pins.h"
+#include <p18f4431.h>
+
+static enum KickerState kickState;
+
+KickerControl kickCon;
+
+void initKicker()
+{
+	K_CHARGE = 0;
+	K_DISCHARGE = 0;
+	K_KICK1 = 0;
+	K_KICK2 = 0;
+	K_KICK3 = 0;
+	K_KICK4 = 0;
+
+	kickState = DISCHARGE;
+
+	kickCon.enable = 0;
+	kickCon.kick1 = 0;
+	kickCon.kick2 = 0;
+	kickCon.kick3 = 0;
+	kickCon.kick4 = 0;
+	kickCon.kick = 0;
+	kickCon.charged = 0;
+	kickCon.updated = 0;
+}
+
+
+void handleKicker()
+{
+	LED2 = !LED2;
+
+	kickCon.updated = 1;
+
+	switch (kickState){
+		case DISCHARGE:
+			K_CHARGE = 0;
+			K_DISCHARGE = 1;
+			K_KICK1 = 0;
+			K_KICK2 = 0;
+			K_KICK3 = 0;
+			K_KICK4 = 0;
+			if (kickCon.enable)
+				kickState = CHARGE;
+			break;
+		case CHARGE:
+			K_CHARGE = 1;
+			K_DISCHARGE = 0;
+			K_KICK1 = 0;
+			K_KICK2 = 0;
+			K_KICK3 = 0;
+			K_KICK4 = 0;
+			if (!kickCon.enable)
+				kickState = DISCHARGE;
+			else if (kickCon.kick)
+				kickState = KICK;
+			else if (K_OVERVOLTAGE)
+				kickState = FULL;
+			break;
+		case FULL:
+			K_CHARGE = 0;
+			K_DISCHARGE = 0;
+			K_KICK1 = 0;
+			K_KICK2 = 0;
+			K_KICK3 = 0;
+			K_KICK4 = 0;
+			if (!kickCon.enable)
+				kickState = DISCHARGE;
+			else if (kickCon.kick)
+				kickState = KICK;
+			else if (!K_OVERVOLTAGE)
+				kickState = CHARGE;
+			break;
+		case KICK:
+			K_CHARGE = 0;
+			K_DISCHARGE = 0;
+			K_KICK1 = kickCon.kick1;
+			K_KICK2 = kickCon.kick1;
+			K_KICK3 = kickCon.kick1;
+			K_KICK4 = kickCon.kick1;
+			kickCon.kick = 0;
+			if (kickCon.enable)
+				kickState = CHARGE;
+			else
+				kickState = DISCHARGE;
+			break;
+		default:
+			// error
+			kickState = DISCHARGE;
+			break;
+	}
+}
