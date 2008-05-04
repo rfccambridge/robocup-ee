08543be340c1cd94b83ec5888aa1ea683d939d2f
Benjamin Johnson <bcfjohns@mit.edu>
2008-05-04 18:59:51 +0000

    Upated pins.h partly.

--- firmware/Auxillery Kicker/pins.h
+++ firmware/Auxillery Kicker/pins.h
@@ -4,16 +4,24 @@
 #define __PINS_H
 
 // === KICKER ===
-#define K_KICK1			LATAbits.LATA0
-#define K_KICK2			LATAbits.LATA4
-#define K_KICK3			LATAbits.LATA5
-#define K_KICK4			LATEbits.LATE0
-#define K_CHARGE		LATEbits.LATE2
+#define K_KICK1			LATcbits.LATc3
+#define K_KICK2			LATDbits.LATD6
+#define K_CHIP1			LATCbits.LATC2
+#define K_CHIP2			LATCbits.LATC1
+#define K_CHARGE		LATDbits.LATD7
+#define K_DISCHARGE		LATBbits.LATB4
 
 // === DEBUG ===
-#define LED1			PORTAbits.RA0
-#define LED2			PORTAbits.RA1
+#define LED1			PORTAbits.LATA0
+#define LED2			PORTAbits.LATA1
+#define LED3			PORTAbits.LATA2
 
+//Mother board LEDs
+#define MBLED1			PORTAbits.LATA3
+#define MBLED2			PORTAbits.LATA4
+
+//Incoming voltages
+#define BattV			PORTAbits.LATA5
 // === DRIBBLER ===
 #define DRIBBLER		PORTCbits.RC2
 
