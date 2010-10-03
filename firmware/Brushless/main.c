9ba27738d58d1e5597e32fd2be54217f5cc2eea4
Benjamin Johnson <bcfjohns@mit.edu>
2010-10-03 23:17:13 +0000

    "Merged with CS199r" This is now the version with the minor loop velocity controllers for the wheels (like in Singapore) and with Error checking code.

--- firmware/Brushless/main.c
+++ firmware/Brushless/main.c
@@ -5,11 +5,11 @@
 // *** set configuration word ***
 #pragma	config OSC      = IRCIO		// internal oscillator
 #pragma	config LVP 	    = OFF		// low voltage programming
-#pragma	config WDTEN    = OFF   	// watchdog timer
+#pragma	config WDTEN    = OFF	   	// watchdog timer
 #pragma	config WDPS     = 256   	// watchdog timer prescaler
 #pragma config BOREN    = ON    	// brown out reset on
 #pragma config BORV     = 42    	// brown out voltage 4.2
-#pragma config FCMEN    = ON  		// fail-safe clock monitor off 
+#pragma config FCMEN    = ON  		// fail-safe clock monitor off
 #pragma config IESO     = ON    	// int/ext switchover off
 #pragma config PWRTEN   = OFF		// powerup timer off
 #pragma	config T1OSCMX  = OFF    	// timer1 osc mux
@@ -47,25 +47,32 @@
 #define LED3			LATEbits.LATE0		// Hall error
 #define LED4			LATAbits.LATA5		// Mosfet driver error
 
-#define min(a,b)        (a<b) ? a : b
+// initial value of timer0, increase for shorter period
+#define TIMER0INIT      32
+
+// this is the maximum error
+#define MAX_ERROR 200
+
+#define min(a,b)        (((a) < (b)) ? (a) : (b))
 #define HALL            (PORTC & 0x07)
 
-#define SPEW_ENCODER      1
-#define DONT_SPEW_ENCODER 0
+// Configuration flags
+#define CFG_SPEW_ENCODER      0b00000001
+#define CFG_SPEW_PKT_STATS    0b00000010
+#define CFG_FEEDBACK          0b00000100
 
-// currently set up to transmit five bytes of Data, so we want 6 of those groups 
-// in a packets (maximum size is 32), so desired size is 30.
-#define DESIRED_PACKET_SIZE 30  
+// Each tx pkt is made up of several sub-packets to minimize transmission overhead 
+// NOTE: total packet size must not exceed MAX_PACKET_SIZE defined in bemixnet.h
+// TODO: doesn't work with more than one sub-packet!!!
+#define NUM_TX_SUBPKTS 1
 
-// initial value of timer0
-// increase for shorter period
-#define TIMER0INIT        32
+unsigned char cfgFlags = 0;
 
 PacketBuffer RxPacket;
 PacketBuffer TxPacket;
 
-unsigned char encoderCount;
-unsigned char encoderFlags;
+unsigned char subPkt = 0; // iterator over the sub-packets
+unsigned int wheel;   // board id
 
 signed int Pconst, Iconst, Dconst;
 signed int previous_error = 0;
@@ -73,14 +80,20 @@ signed int Iterm = 0;
 signed char command = 0;
 unsigned char direction = 0;
 
-void commutateMotor();
-void high_ISR();	 //Interrupt Service Routine
+// Ben's minor loop velocity controller
+signed long int m = 0;
+signed long int m_n1 = 0; //from from the last iteration
+signed long int e2 = 0;
+signed long int e2_n1 = 0; //e2 from the last iteration
+
+static void blinkLEDs(void);
+static void commutateMotor(void);
+//Interrupt Service Routines
+void high_ISR(void);
 void handleQEI(PacketBuffer * TxPacket);
 
 void main()
 {
-	unsigned char timer, timer2;
-	
 	TRISE = 0xf8;
 
 	// *** 8 MHz clock ***
@@ -95,7 +108,7 @@ void main()
 	PTPERL = 0xff;
 	
 	// *** Configure IO ***
-	ANSEL0 = 0x00;//ANSEL0 = 0x01;						// AN0 analog, all others digital
+	ANSEL0 = 0x00;//ANSEL0 = 0x01;		// AN0 analog, all others digital
 	ANSEL1 = 0x00;
 	TRISA = 0xdf;
 	TRISB = 0xff;
@@ -110,39 +123,34 @@ void main()
 	// 101          8.2ms
 	// 100          4.1ms
 
-	T0CON = 0b11000101;
-	INTCON = 0b11100000;
+	T0CON = 0b11000101; //enabled, 8bit, internal clock, low->high transition, Use of prescaler, 1:64
+	INTCON = 0b11100000; 
 
 	// *** Initialize encoder ***
 	QEICON = 0b00011000;
 
-	// *** Blink LEDs ***
-	LED1 = 1;
-	LED2 = 1;
-	LED3 = 1;
-	for (timer2=0; timer2<16; timer2++)
-		for (timer=0; timer<255; timer++);
-	LED1 = 0;
-	LED2 = 0;
-	LED3 = 0;
-	for (timer2=0; timer2<16; timer2++)
-		for (timer=0; timer<255; timer++);
-	LED1 = 1;
-	LED2 = 1;
-	LED3 = 1;
+	cfgFlags = CFG_FEEDBACK; // | CFG_SPEW_ENCODER;
+
+	blinkLEDs();
+
+	/* Figure out which wheel this is 
+	 0 -- Front right
+	 1 -- Front left
+   	 2 -- Rear left
+	 3 -- Rear right
+	*/
+	wheel = PORTAbits.AN0 + 2*PORTAbits.AN1;
 
 	// *** Configure serial ***
 	// (this needs to be last)
 	initRx(&RxPacket);
 	initTx(&TxPacket);
-
-	encoderFlags = DONT_SPEW_ENCODER;
-	encoderCount = 0;
+	subPkt = 0;
 
 	// defaults for testing
-	Pconst = 12;
+	Pconst = 50;
 	Dconst = 3;
-	Iconst = 3;
+	Iconst = 1;
 	command = 0;
 	Iterm = 0;
 	previous_error = 0;
@@ -161,39 +169,39 @@ void main()
 		}
 
 		// Check for dead hall
-		LED3 = 1;
-		if (HALL == 0 || HALL == 7)
-			LED3 = 0;
+	//	LED3 = 1;
+	//	if (HALL == 0 || HALL == 7)
+	//		LED3 = 0;
 
-		
-
-		if (encoderFlags==SPEW_ENCODER && encoderCount == DESIRED_PACKET_SIZE) 
-		{
-			TxPacket.length = DESIRED_PACKET_SIZE;
+		// TxPacket.done is not set while the packet is in transmission
+		if ((cfgFlags & CFG_SPEW_ENCODER) && TxPacket.done && subPkt == NUM_TX_SUBPKTS) {
 			transmit(&TxPacket);
-			encoderCount = 0;
+			subPkt = 0;
 		}
-		
-		if (RxPacket.done) {
+
+		if (RxPacket.done && RxPacket.address == 'w') {
 			ClrWdt();
 			RxPacket.done = 0;
 			switch(RxPacket.port) {
 				case 'r':
 					Reset();
 				case 'w':
-					// update wheel speed			
-					command = RxPacket.data[PORTAbits.AN0 + 2*PORTAbits.AN1];//RxPacket.data[0];							
+					// Get the transmitted wheel speed			
+					command = RxPacket.data[wheel];
 					break;
 				case 'f':
 					Pconst = (signed int) RxPacket.data[0];
 					Iconst = (signed int) RxPacket.data[1];
 					Dconst = (signed int) RxPacket.data[2];
 					break;
-				case 'e':
-					//if (RxPacket.data[0]=='1')//&& RxPacket.data[1] == PORTAbits.AN0 + 2*PORTAbits.AN1)
-					//	encoderFlags = DONT_SPEW_ENCODER;
-					//else
-					encoderFlags = SPEW_ENCODER;
+				case 'c': // set config flags
+					cfgFlags &= ~CFG_SPEW_ENCODER; // special: make spewing flag exclusive to one board
+					if (RxPacket.data[0] == wheel)
+						cfgFlags = RxPacket.data[1];
+					break;
+				case 's': // temporary value - make this work
+					// TODO: estimate wheel speed
+					break;
 				default:
 					break;	
 			}
@@ -203,16 +211,26 @@ void main()
 
 // OVCOND = mask
 // OVCONS = value when masked
-void commutateMotor()
+void commutateMotor(void)
 {
 	const unsigned char backdrive[8] = { 0b00000000, 	// 0 error
-										0b00100001, 	// 4
+										 0b00010001,	//these are new commutation order, didn't fix direction biase problem :(
+										 0b00100010,
+										 0b00100001,
+										 0b00001100,
+										 0b00010100,
+										 0b00001010,
+									 	 0b00000000}; 	// 7 error
+
+
+
+										/*0b00100001, 	// 4
 										0b00001010, 	// 2		
 										0b00100010, 	// 6
 										0b00010100, 	// 1
 										0b00010001, 	// 5
 										0b00001100, 	// 3
-										0b00000000}; 	// 7 error
+										0b00000000}; 	// 7 error*/
 	const unsigned char fordrive[8] = {0b00000000, 	// 0 error
 										0b00001010,		// 4
 										0b00010100,		// 2
@@ -227,15 +245,20 @@ void commutateMotor()
 	// to prevent glitching
 	unsigned char _OVDCOND;
 	unsigned char _OVDCONS;
-
+	if (command == 0 && !(cfgFlags & CFG_FEEDBACK)){ // if command is zero we want to coast! (Not for use with feedback)
+		_OVDCOND = fordrive[0];
+		_OVDCONS = ~fordrive[0];
+	} else 
 	if (direction==0) {
 		// PWM high side
+	//	LED4 = 0;
 		_OVDCOND = fordrive[hall] & 0b00111000;
 		// turn on low side
 		_OVDCONS = ~(fordrive[hall] & 0b00000111);
 	} else if (direction == 1){
 		_OVDCOND = backdrive[hall] & 0b00111000;
 		_OVDCONS = ~(backdrive[hall] & 0b00000111);
+	//	LED4 = 1;
 	}
 
 	OVDCOND = _OVDCOND;
@@ -244,14 +267,19 @@ void commutateMotor()
 
 
 
-void handleQEI(PacketBuffer * encoderPacket)
+void handleQEI(PacketBuffer * txPkt)
 {
 	unsigned int encoderCentered = 0;
-	signed char encoder = 0;
-	signed int error = 0;
-	signed int duty = 0;
-	unsigned char dutyHigh, dutyLow;
-	signed int Dterm;
+	signed int encoder = 0;
+	signed long int error = 0;
+	signed int duty = 0;			// modifies speed based on PID feedback
+	unsigned char dutyHigh, dutyLow;// high and low bits for duty cycle
+	
+	signed char encHigh;
+	signed char encLow;
+	signed long int command2;
+	
+	TMR0L = TIMER0INIT;
 
 	// read and reset position accumulator
 	encoderCentered = POSCNTH;
@@ -259,55 +287,31 @@ void handleQEI(PacketBuffer * encoderPacket)
 	encoderCentered += POSCNTL;
 	POSCNTH = 0x80;
 	POSCNTL = 0x00;
-
-	TMR0L = TIMER0INIT;
-
-	// convert encoder value to 8 bit 2's comp
-	//if (encoderCentered >= 0x8800) encoderCentered = 0x8800-1;
-	//if (encoderCentered <= 0x7800) encoderCentered = 0x7800+1;
-
-    if (encoderCentered >= 0x8400) encoderCentered = 0x8400-1;
-	if (encoderCentered <= 0x7c00) encoderCentered = 0x7c00+1;
 		
     if (encoderCentered >=0x8000)
-		encoder = (encoderCentered - 0x8000)/ 4;
-	else
-		encoder = -(signed char)((0x8000-encoderCentered)/ 4);
+		encoder = (signed int)(encoderCentered - 0x8000);
+	else									
+		encoder = -(signed int)(0x8000-encoderCentered);
 
-	// calculate error, check for rollover
-	error = ((signed int) encoder) - ((signed int) command);
-	duty = command; //This is to be able to test the dynamics of the robots, without the PID affecting things
-/*	
-	duty = error * Pconst / 3;
-	Dterm = Dconst * (error - previous_error) / 3;
-	Iterm += Iconst * error / 3;
+	encoder = - encoder;
 
-	//check things are small
-	if (duty > 900){
-		duty = 900;
-	} else if (duty < -900){
-		duty = -900;
-	}
-
-	if (Dterm > 900){
-		Dterm = 900;
-	} else if (Dterm < -900){
-		Dterm = -900;
-	}
-
-	if (command == 0 && encoder == 0)
-		Iterm = 0;
-	if (Iterm > 500){
-		Iterm = 500;
-	} else if (Iterm < -500){
-		Iterm = -500;
-	}
-
-	duty += Dterm + Iterm;
-*/
-	if(duty > 1023) duty = 1023;
-	if(duty < -1023) duty = -1023;
+	//Do the controls math;
+	command2 = ((signed long int)command)*4;
+	error = command2 - (signed long int)encoder;
+	m = error + m_n1;
+	e2 = 3*m-encoder;
+	duty = 2*(e2-e2_n1)+e2;//
+//	if (encoder == 0 && command2 == 0) duty = 0; //to help with the nonlinearity;
 	
+	m_n1 = m;
+	e2_n1 = e2;
+	
+//	duty = command;
+	//duty = duty*8;
+
+	if(duty > 1023) duty = 1023;
+	if(duty < -1023) duty = -1023;	
+
 	// convert to 10 bit sign magnitude
 	if (duty >= 0) {
 		direction = 1;
@@ -316,10 +320,7 @@ void handleQEI(PacketBuffer * encoderPacket)
 		direction = 0;
 	}
 
-	// hacked, weird around 0, don't change
-	if (duty >= 1020)
-		duty=1020;
-	duty = 1020 - duty;
+	duty = 1023 - duty;
 
 	dutyHigh = duty >> 8;
 	dutyLow = duty;
@@ -332,27 +333,35 @@ void handleQEI(PacketBuffer * encoderPacket)
 	PDC2H = dutyHigh;
 	PDC2L = dutyLow;
 
+	
 	// put data in transmit buffer
-	if (encoderFlags==SPEW_ENCODER && encoderCount < DESIRED_PACKET_SIZE) {
-		encoderPacket->address = '2';
-		encoderPacket->port = 'a';
-		encoderPacket->data[encoderCount++] = POSCNTH;
-		encoderPacket->data[encoderCount++] = POSCNTL;
-		encoderPacket->data[encoderCount++] = dutyHigh;
-		encoderPacket->data[encoderCount++] = dutyLow;
-		encoderPacket->data[encoderCount++] = command;
+	if ((cfgFlags & CFG_SPEW_ENCODER) && txPkt->done && subPkt < NUM_TX_SUBPKTS) {
+		// assemble one sub-packet
+		txPkt->data[txPkt->length++] = encoder >> 8;
+		txPkt->data[txPkt->length++] = encoder;
+		txPkt->data[txPkt->length++] = duty >> 8;
+		txPkt->data[txPkt->length++] = duty;
+		txPkt->data[txPkt->length++] = command2 / 4;
+		if (cfgFlags & CFG_SPEW_PKT_STATS) {
+			txPkt->data[txPkt->length++] = pktsReceived;
+			txPkt->data[txPkt->length++] = pktsAccepted;
+			txPkt->data[txPkt->length++] = pktsMismatched;
+		}
+		subPkt++;
 	}
 
-	previous_error = error;
+
+	LED3=!LED3;
 }
 
-#pragma code high_vector=0x08				//We are not using Priortized Interrupts: so all interrupts go to 0x08. 
+#pragma code high_vector=0x08 	// We are not using Priortized Interrupts: 
+								// so all interrupts go to 0x08. 
 void interrupt_high_vector(){
-	_asm GOTO high_ISR _endasm				//branching to the actual ISR
+	_asm GOTO high_ISR _endasm	//branching to the actual ISR
 }
 #pragma code
 
-#pragma interrupt high_ISR					 //Interrupt Service Routine (the real one)
+#pragma interrupt high_ISR		// Interrupt Service Routine (the real one)
 void high_ISR()
 {
 	if (INTCONbits.TMR0IE && INTCONbits.TMR0IF) {
@@ -360,16 +369,36 @@ void high_ISR()
 		INTCONbits.TMR0IF = 0;
 		handleQEI(&TxPacket);
 		LED1 = 1;
-	} else if (PIE1bits.RCIE && PIR1bits.RCIF) {
+	}
+	if (PIE1bits.RCIE && PIR1bits.RCIF) {
 		LED2 = 0;
 		PIR1bits.RCIF = 0;
 		handleRx(&RxPacket);
 		LED2 = 1;
-	} else if (PIE1bits.TXIE && PIR1bits.TXIF) {
+	}
+	if (PIE1bits.TXIE && PIR1bits.TXIF) {
 		PIR1bits.TXIF = 0;
 		handleTx(&TxPacket);
 	} 
 }
-#pragma
+
+void blinkLEDs(void)
+{
+	unsigned char timer, timer2;
+
+	LED1 = 1;
+	LED2 = 1;
+	LED3 = 1;
+	for (timer2=0; timer2<16; timer2++)
+		for (timer=0; timer<255; timer++);
+	LED1 = 0;
+	LED2 = 0;
+	LED3 = 0;
+	for (timer2=0; timer2<16; timer2++)
+		for (timer=0; timer<255; timer++);
+	LED1 = 1;
+	LED2 = 1;
+	LED3 = 1;
+}
 
 
