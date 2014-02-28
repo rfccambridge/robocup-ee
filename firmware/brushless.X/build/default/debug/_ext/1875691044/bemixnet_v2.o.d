f9e33a2486b18e864be9ebae4ece80c2b56d02df
Erik Schluntz <eschluntz@college.harvard.edu>
2014-02-27 19:33:42 -1000

    added mplab x project configuration

---
+++ firmware/brushless.X/build/default/debug/_ext/1875691044/bemixnet_v2.o.d
@@ -0,0 +1,4 @@
+build/default/debug/_ext/1875691044/bemixnet_v2.o:  \
+ ../Brushless/bemixnet_v2.c  \
+../Brushless/bemixnet_v2.h  \
+ ../Brushless/pins_v2.h 
---
+++ firmware/brushless.X/build/default/production/_ext/1875691044/main_v2.o.d
@@ -0,0 +1,4 @@
+build/default/production/_ext/1875691044/main_v2.o:  \
+ ../Brushless/main_v2.c  \
+../Brushless/bemixnet_v2.h  \
+../Brushless/pins_v2.h 
