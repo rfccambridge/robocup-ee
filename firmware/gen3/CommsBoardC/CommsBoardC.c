663a0b365b68853d93b0c81c92761f0f5c2ba0d4
Karl Otness <karlotness@college.harvard.edu>
2015-04-18 18:17:42 -0400

    Add serial init sequence to CommsBoard.c

--- firmware/gen3/CommsBoardC/CommsBoardC.c
+++ firmware/gen3/CommsBoardC/CommsBoardC.c
@@ -12,9 +12,22 @@
 #include "serial.h"
 #include "spi.h"
 
+#define FOSC 1843200
+#define BAUD 9600
+#define MYUBRR FOSC/16/BAUD-1
+
 int main(void)
 {
+	/* Initialize serial, move this to serial lib */
+	unsigned int ubrr = MYUBRR;
+	UBRR0H = (unsigned char)(ubrr>>8);
+	UBRR0L = (unsigned char)ubrr;
+	UCSR0B = (1 << RXEN0) | (1 << TXEN0);
+	UCSR0C = (1 << USBS0) | (3<< UCSZ00);
+	
+	DDRA = 0xFF;
 	message recvMsg;
+	unsigned char last = 0;
     while(true)
     {
 		// Figure out how many message we might want to fetch
@@ -28,22 +41,19 @@ int main(void)
 				// Break out.
 				break;
 			}
-			// Have a message ready. Forward it over SPI.
-			if(!spiPushOutbox(&recvMsg)){
-				// Failed to send message.
-				// Break to not cause more damage
-				// Nothing to be done to recover the other message
-				break;
+			// Process the message
+			if(recvMsg.message[0])
+			{
+				// Turn on the LED
+				PORTA = 0xFF;
+			}
+			else
+			{
+				// Turn off the LED
+				PORTA = 0x00;
 			}
 		}
-		inboxSize = spiGetInboxSize();
-		for(int i = 0; i < inboxSize; i++){
-			if(!spiPopInbox(&recvMsg)){
-				break;
-			}
-			if(!serialPushOutbox(&recvMsg)){
-				break;
-			}
-		}
+		recvMsg.slaveID = 0;
+		recvMsg.message[0] = last++;
     }
 }
