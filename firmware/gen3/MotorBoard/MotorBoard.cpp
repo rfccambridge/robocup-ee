1b64fc68ddaac6739e10877e75586e3228475b23
Ezra Zigmond <ezrazigmond@gmail.com>
2015-04-22 14:28:15 -0700

    More pwm tests

--- firmware/gen3/MotorBoard/MotorBoard.cpp
+++ firmware/gen3/MotorBoard/MotorBoard.cpp
@@ -1,12 +1,18 @@
 #include <avr/io.h>
 
 #include "SPISlave.h"
+#include <util/delay.h>
 
 int main(void)
 {
-	// Set timer to fast PWM mode, with no prescaling, set on compare match,
-	TCCR0A |= (1 << WGM00) | (1 << WGM01) | (1 << CS00) | (1 << COM0A0) | (1 << COM0A1);
+	// Enable system clock setting
+	CLKPR = (1 << CLKPCE);
 	
+	
+	CLKPR = (1 << CLKPS0);
+	// Set timer to fast PWM mode, with no prescaling, clear on compare match,
+	TCCR0A |= (1 << WGM00) | (1 << WGM01) | (1 << CS00) | (1 << COM0A1);
+	TCNT0 =  
 	// Set OC0A as output
 	DDRB |= (1 << PINB4);
 	
@@ -16,6 +22,8 @@ int main(void)
 	SPISlave spi;
     while(1)
     {
+		_delay_ms(500);
+		OCR0A += 10;
 		/*
 		spi.ReceiveSPI();
 		
