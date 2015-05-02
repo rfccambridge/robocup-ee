1958ff0f4f961c11976deb0dd43ade72a5807334
Karl Otness <karlotness@college.harvard.edu>
2015-05-02 19:33:50 -0400

    Adjust clock timing for comms board

--- firmware/gen3/CommsBoardC/CommsBoardC.c
+++ firmware/gen3/CommsBoardC/CommsBoardC.c
@@ -8,35 +8,42 @@
 #include <avr/io.h>
 #include <string.h>
 #include <stdbool.h>
+#include <avr/interrupt.h>
+#include <avr/common.h>
 #include "serial.h"
 #include "spi.h"
 
 #define FOSC 1843200
+#define F_CPU FOSC
 #define BAUD 9600
-#define MYUBRR FOSC/16/BAUD-1
+#define MYUBRR F_CPU/16/BAUD-1
 
 int main(void)
 {
+	CLKPR = (1 << CLKPCE);
+	CLKPR |= (1 << CLKPS0);
 	int counter = 0;
 	// Initialize serial, move this to serial lib
 	unsigned int ubrr = MYUBRR;
 	UBRR0H = (unsigned char)(ubrr>>8);
 	UBRR0L = (unsigned char)ubrr;
-	UCSR0B = (1 << RXEN0) | (1 << TXEN0);
-	UCSR0C = (1 << USBS0) | (3<< UCSZ00);
+	UCSR0C = (0 << UMSEL0) | (0 << UPM00) | (0 << UPM01) | (1 << USBS0) | (1 << UCSZ01) | (1 << UCSZ00);
+	UCSR0B = 0x00;
 	UCSR0B |= (1 << UDRIE0);
 	UCSR0B |= (1 << TXCIE0);
 	UCSR0B |= (1 << UDRIE0) | (1 << TXCIE0);
+	UCSR0B |= (1 << RXEN0) | (1 << TXEN0);
 	
 	DDRC = 0xFF;
 	message recvMsg;
+	memset(&recvMsg, 0, sizeof(message));
 	unsigned char last = 0;
 	PORTC = 0x00;
+	
+	sei();
+	
     while(1)
     {
-		if(!(counter++ % 1000)){
-			PORTC ^= 0x01;
-		}
 		// Figure out how many message we might want to fetch
 		// This way, even if new message come as we're looping
 		// The loop will still end
@@ -48,6 +55,7 @@ int main(void)
 				// Break out.
 				break;
 			}
+			PORTC ^= 0b00000100;
 			// Process the message
 			if(recvMsg.message[0])
 			{
@@ -60,8 +68,11 @@ int main(void)
 				PORTC &= ~(0x01);
 			}
 		}
-		recvMsg.slaveID = 0;
-		recvMsg.message[0] = last++;
-		serialPushOutbox(&recvMsg);
+			PORTC ^= 0b00001000;
+			recvMsg.slaveID = 0xFF;
+			recvMsg.message[0] =  0xFF;
+			recvMsg.message[1] = 0xFF;
+			recvMsg.message[2] = 0xFF;
+			serialPushOutbox(&recvMsg);
     }
 }
