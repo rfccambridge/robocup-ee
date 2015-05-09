411bd19b5b14b9fdb985e311e9fd8da30b86d3d9
Ezra Zigmond <ezrazigmond@gmail.com>
2015-05-09 15:14:20 -0700

    Added skeleton kickerboard project

---
+++ firmware/gen3/KickerBoard/KickerBoard.cpp
@@ -0,0 +1,19 @@
+/*
+ * KickerBoard.cpp
+ *
+ * Created: 5/9/2015 3:11:00 PM
+ *  Author: Ezra
+ */ 
+
+
+#include <avr/io.h>
+#include "SPISlave.h"
+#include "EELib.h"
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
