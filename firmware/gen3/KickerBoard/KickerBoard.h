7a203747d2820f53ee0f23eba0685bc3f32ba0b8
Ezra Zigmond <ezrazigmond@gmail.com>
2015-05-25 14:05:56 -0700

    Added pin names for kickerboard

---
+++ firmware/gen3/KickerBoard/KickerBoard.h
@@ -0,0 +1,28 @@
+// Pin names
+
+#define CHARGE PINA0
+#define DISCHARGE PINA1
+#define KICK_ENABLE PINA2
+#define CHIP_ENABLE PINA3
+#define DONE PINA4
+
+#define SS_K PINB0
+#define SCK PINB1
+#define MOSI PINB2
+#define MISO PINB3
+#define PWM PINB5
+#define DRIB PINB6
+#define BB_OUT PINB7
+
+#define RLED1 PINB1
+#define GLED1 PINB2
+#define RLED2 PINB3
+#define GLED2 PINB4
+#define RLED3 PINB5
+#define GLED3 PINB6
+
+#define DIVCAP PINF0
+#define BATSENSE PINF1
+#define BB_IN PINF2
+
+#define RESET PING5
