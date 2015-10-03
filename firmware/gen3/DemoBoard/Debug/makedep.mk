d31c19f46cd44f86bd619f5b960722f1ef3f3c58
Ezra <ezrazigmond@gmail.com>
2015-10-03 17:07:03 -0400

    Made a real .gitignore and removed binary files that had been commited

--- firmware/gen3/DemoBoard/Debug/makedep.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit or delete the file
-################################################################################
-
-src\ASF\common\boards\user_board\init.c
-
-src\main.c
-
