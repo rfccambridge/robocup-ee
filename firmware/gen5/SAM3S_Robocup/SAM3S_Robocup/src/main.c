1c91fe876f32bbefd81f50ed347b7fecbc52e704
ljmulshine <lmulshine@college.harvard.edu>
2016-09-27 14:52:59 -0400

    Added some comments

--- firmware/gen5/SAM3S_Robocup/SAM3S_Robocup/src/main.c
+++ firmware/gen5/SAM3S_Robocup/SAM3S_Robocup/src/main.c
@@ -1,33 +1,9 @@
 /**
- * \file
+ * Author: Liam Mulshine & ___
  *
- * \brief Empty user application template
- *
+ * Robocup Firmware
  */
 
-/**
- * \mainpage User Application template doxygen documentation
- *
- * \par Empty user application template
- *
- * Bare minimum empty user application template
- *
- * \par Content
- *
- * -# Include the ASF header files (through asf.h)
- * -# "Insert system clock initialization code here" comment
- * -# Minimal main function that starts with a call to board_init()
- * -# "Insert application code here" comment
- *
- */
-
-/*
- * Include header files for all drivers that have been imported from
- * Atmel Software Framework (ASF).
- */
-/*
- * Support and FAQ: visit <a href="http://www.atmel.com/design-support/">Atmel Support</a>
- */
 #include <asf.h>
 #include <RobocupDrivers.h>
 
