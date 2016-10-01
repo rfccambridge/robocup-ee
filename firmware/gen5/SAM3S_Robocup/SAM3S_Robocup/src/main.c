4a6471c0b4ad18417f98e04ab92560e56a0177c6
ljmulshine <lmulshine@college.harvard.edu>
2016-10-01 17:30:03 -0400

    Motor module closer to complete. We can now have 4 pwm signals running consistently and are able to change them! Yahoo!

--- firmware/gen5/SAM3S_Robocup/SAM3S_Robocup/src/main.c
+++ firmware/gen5/SAM3S_Robocup/SAM3S_Robocup/src/main.c
@@ -9,6 +9,10 @@
 
 #define MY_LED IOPORT_CREATE_PIN(PIOA, 8)
 
+extern pwm_channel_t pwm_0;
+extern pwm_channel_t pwm_1;
+
+
 int main (void)
 {
 	/* Insert system clock initialization code here (sysclk_init()). */
@@ -19,16 +23,21 @@ int main (void)
 	ioport_set_pin_dir(MY_LED, IOPORT_DIR_OUTPUT);
 	ioport_set_pin_level(MY_LED,LOW);
 		
+	//	REG_PWM_FCR = 1;	// clear the pwm fault if you accidentally set pwm period or duty cycle to an bad value :)	
+
 	// configure pwm module to allow for motor control
-   //  configure_pwm();
+	configure_pwm();
 	
-	//Motor motor1;
-	//initialize_motor(&motor1,CHANNEL0);
-	test_pwm();
+	Motor motor0;
+	Motor motor1;
+	Motor motor2;
+	Motor motor3;
+	initialize_motor(&motor0,CHANNEL0);
+	initialize_motor(&motor1,CHANNEL1);
+	initialize_motor(&motor2,CHANNEL2);
+	initialize_motor(&motor2,CHANNEL3);
 	
-	if(!pmc_is_periph_clk_enabled(ID_PWM))
-		while(1);
-	else 
-		while(1);
+    //test_pwm();
+	while(1);
 	/* Insert application code here, after the board has been initialized. */
 }
