19bad2a0fe294191d5ee16cf8a307cc4f0bac0b8
Damian <damian@email.com>
2016-10-09 19:05:31 +0000

    Streamlined Build Environment Setup

---
+++ README.md
@@ -0,0 +1,7 @@
+## Build Environment Installation
+
+- `source /opt/ros/kinetic/setup.bash`
+- `cd <your robocup directory>/catkin_ws/src`
+- `catkin_init_workspace`
+- `cd <your robocup directory>/catkin_ws`
+- `catkin_make`
--- catkin_ws/build/using_markers/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ljmulshine/robocup-ee/catkin_ws/src/using_markers
-# Build directory: /home/ljmulshine/robocup-ee/catkin_ws/build/using_markers
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
