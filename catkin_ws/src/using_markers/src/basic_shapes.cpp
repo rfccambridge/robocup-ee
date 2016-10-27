fcb365ab34af9ce254cd5a5a46121004cb5245f3
John Smith <john@kateritsa.tk>
2016-10-27 19:08:36 -0400

    Made primary progress in moving a marker with incoming messages

--- catkin_ws/src/using_markers/src/basic_shapes.cpp
+++ catkin_ws/src/using_markers/src/basic_shapes.cpp
@@ -1,100 +1,43 @@
-/*
- * Copyright (c) 2010, Willow Garage, Inc.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the Willow Garage, Inc. nor the names of its
- *       contributors may be used to endorse or promote products derived from
- *       this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
 
-// %Tag(FULLTEXT)%
 #include <ros/ros.h>
+#include "std_msgs/Int8.h"
 #include <visualization_msgs/Marker.h>
 
 #include <cmath>
 
-int main( int argc, char** argv )
+//TODO: This is BAD!!!
+int x_pos = 0;
+
+void subscriber_callback(const std_msgs::Int8 i)
 {
+  printf("Received %d\n", x_pos = i.data);
+}
+
+int main(int argc, char **argv)
+{
+  //Initialize our ROS system
   ros::init(argc, argv, "points_and_lines");
+
   ros::NodeHandle n;
+  ros::Rate loop_rate(30);
   ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 10);
 
-  ros::Rate r(30);
+  //Provides data for the coords of our marker
+  ros::Subscriber sub = n.subscribe("chatter", 1000, subscriber_callback);
 
-  float f = 0.0;
 
   uint32_t shape = visualization_msgs::Marker::CUBE;
-  while (ros::ok())
+  while(ros::ok())
   {
-// %Tag(MARKER_INIT)%
-    visualization_msgs::Marker points, line_strip, line_list, marker;
-    marker.header.frame_id = points.header.frame_id = line_strip.header.frame_id = line_list.header.frame_id = "/my_frame";
-    marker.header.stamp = points.header.stamp = line_strip.header.stamp = line_list.header.stamp = ros::Time::now();
-    marker.ns = points.ns = line_strip.ns = line_list.ns = "basic_shapes";
-    points.action = line_strip.action = line_list.action = visualization_msgs::Marker::ADD;
-    points.pose.orientation.w = line_strip.pose.orientation.w = line_list.pose.orientation.w = 1.0;
-// %EndTag(MARKER_INIT)%
-
-// %Tag(ID)%
-    points.id = 0;
-    line_strip.id = 1;
-    line_list.id = 2;
-    marker.id = 3;
-// %EndTag(ID)%
-
-// %Tag(TYPE)%
-    points.type = visualization_msgs::Marker::POINTS;
-    line_strip.type = visualization_msgs::Marker::LINE_STRIP;
-    line_list.type = visualization_msgs::Marker::LINE_LIST;
+    visualization_msgs::Marker marker;
+    marker.header.frame_id = "/my_frame";
+    marker.header.stamp = ros::Time::now();
+    marker.ns = "basic_shapes";
+    marker.id = 0;
     marker.type = shape;
-// %EndTag(TYPE)%
-
-// %Tag(SCALE)%
-    // POINTS markers use x and y scale for width/height respectively
-    points.scale.x = 0.0;
-    points.scale.y = 0.0;
-
-    // LINE_STRIP/LINE_LIST markers use only the x component of scale, for the line width
-    line_strip.scale.x = 0.1;
-    line_list.scale.x = 0.0;
-// %EndTag(SCALE)%
-
-// %Tag(COLOR)%
-    // Points are green
-    points.color.g = 0.0f;
-    points.color.a = 0.0;
-
-    // Line strip is blue
-    line_strip.color.b = 1.0;
-    line_strip.color.a = 1.0;
-
-    // Line list is red
-    line_list.color.r = 1.0;
-    line_list.color.a = 1.0;
-// %EndTag(COLOR)%
 
     // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
-    marker.pose.position.x = 0;
+    marker.pose.position.x = x_pos;
     marker.pose.position.y = 0;
     marker.pose.position.z = 0;
     marker.pose.orientation.x = 0.0;
@@ -114,37 +57,12 @@ int main( int argc, char** argv )
     marker.color.a = 1.0;
 
     marker.lifetime = ros::Duration();
-    
-    
 
-// %Tag(HELIX)%
-    // Create the vertices for the points and lines
-    for (uint32_t i = 0; i < 5; ++i)
-    {
-      float x = 5;
-      float y = i;
-      float z = 0;
-
-      geometry_msgs::Point p;
-      p.x = x;
-      p.y = y;
-      p.z = z;
-
-      points.points.push_back(p);
-      line_strip.points.push_back(p);
-
-      // The line list needs two points for each line
-      line_list.points.push_back(p);
-      p.z += 1.0;
-      line_list.points.push_back(p);
-    }
-// %EndTag(HELIX)%
-    marker_pub.publish(points);
-    marker_pub.publish(line_strip);
-    marker_pub.publish(line_list);
+    //Publish the marker
     marker_pub.publish(marker);
 
-    r.sleep();
+    //Process all of the callbacks and sleep a bit between loops
+    ros::spinOnce();
+    loop_rate.sleep();
   }
 }
-// %EndTag(FULLTEXT)%
--- catkin_ws/src/using_markers/src/basic_shapes_publisher.cpp
+++ catkin_ws/src/using_markers/src/basic_shapes_publisher.cpp
@@ -2,78 +2,34 @@
 #include "ros/ros.h"
 #include "std_msgs/Int8.h"
 
-/**
- * This tutorial demonstrates simple sending of messages over the ROS system.
- */
 int main(int argc, char **argv)
 {
-  /**
-   * The ros::init() function needs to see argc and argv so that it can perform
-   * any ROS arguments and name remapping that were provided at the command line.
-   * For programmatic remappings you can use a different version of init() which takes
-   * remappings directly, but for most command-line programs, passing argc and argv is
-   * the easiest way to do it.  The third argument to init() is the name of the node.
-   *
-   * You must call one of the versions of ros::init() before using any other
-   * part of the ROS system.
-   */
+  //Initialize our ROS system
   ros::init(argc, argv, "int8_publisher");
 
-  /**
-   * NodeHandle is the main access point to communications with the ROS system.
-   * The first NodeHandle constructed will fully initialize this node, and the last
-   * NodeHandle destructed will close down the node.
-   */
   ros::NodeHandle n;
-
-  /**
-   * The advertise() function is how you tell ROS that you want to
-   * publish on a given topic name. This invokes a call to the ROS
-   * master node, which keeps a registry of who is publishing and who
-   * is subscribing. After this advertise() call is made, the master
-   * node will notify anyone who is trying to subscribe to this topic name,
-   * and they will in turn negotiate a peer-to-peer connection with this
-   * node.  advertise() returns a Publisher object which allows you to
-   * publish messages on that topic through a call to publish().  Once
-   * all copies of the returned Publisher object are destroyed, the topic
-   * will be automatically unadvertised.
-   *
-   * The second parameter to advertise() is the size of the message queue
-   * used for publishing messages.  If messages are published more quickly
-   * than we can send them, the number here specifies how many messages to
-   * buffer up before throwing some away.
-   */
   ros::Publisher chatter_pub = n.advertise<std_msgs::Int8>("chatter", 1000);
-
   ros::Rate loop_rate(10);
 
-  /**
-   * A count of how many messages we have sent. This is used to create
-   * a unique string for each message.
-   */
-  int count = 0;
-  while (ros::ok())
+  int count = 0, dir = 1;
+  while(ros::ok())
   {
-    /**
-     * This is a message object. You stuff it with data, and then publish it.
-     */
+    //Create and populate a message to send
     std_msgs::Int8 msg;
+    msg.data = count += dir;
 
-    msg.data = count++;
-
-    printf("Publishing %d\n", msg.data);
-
-    /**
-     * The publish() function is how you send messages. The parameter
-     * is the message object. The type of this object must agree with the type
-     * given as a template parameter to the advertise<>() call, as was done
-     * in the constructor above.
-     */
     chatter_pub.publish(msg);
 
+    //Process all of the callbacks and sleep a bit between loops
     ros::spinOnce();
-
     loop_rate.sleep();
+
+    //Bounce count within -3 and 3
+    if(count == -3)
+      dir = 1;
+    else if(count == 3)
+      dir = -1;
+
   }
 
   return 0;
